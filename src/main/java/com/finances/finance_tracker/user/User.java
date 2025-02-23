ecfe02aa91a4018f8d9b829b6b9ca03be3d43b86
Nick Boysko <boysko.1@osu.edu>
2025-02-23 15:29:16 -0500

    Ran the user class which created the table in the database

---
+++ src/main/java/com/finances/finance_tracker/user/User.java
@@ -0,0 +1,32 @@
+package com.finances.finance_tracker.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+    
+}
