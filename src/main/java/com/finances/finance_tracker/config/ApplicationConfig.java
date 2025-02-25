5e408b54a4cfeba619e53995c0aaccf7aef4d048
Nick Boysko <boysko.1@osu.edu>
2025-02-24 21:28:46 -0500

    Finished the jwt authentication filter and added the application config file

---
+++ src/main/java/com/finances/finance_tracker/config/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package com.finances.finance_tracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.finances.finance_tracker.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+}
